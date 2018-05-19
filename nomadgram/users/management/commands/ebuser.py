ea12e3e472dd740f384675762fdaca89fb2e3043
HYOUNGBIN <jumice10@gmail.com>
2018-05-20 00:29:00 +0900

    superuser command added2

--- nomadgram/users/management/commands/ebuser.py
+++ nomadgram/users/management/commands/ebuser.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
 
-class command(BaseCommand):
+class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
