4855d127e596c7700b517355a7b2db72352b3c40
HYOUNGBIN <jumice10@gmail.com>
2018-05-20 02:33:28 +0900

    notification push code added

--- nomadgram/notifications/views.py
+++ nomadgram/notifications/views.py
@@ -29,4 +29,34 @@ def create_notification(creator, to, notification_type, image=None, comment=None
         comment=comment
     )
 
+    time.sleep(10)
+
     notification.save()
+
+    action = ''
+
+    if notification_type == 'like':
+
+        action = 'liked your photo'
+
+    elif notification_type == 'comment':
+
+        action = 'commented on your photo'
+
+    elif notification_type == 'follow':
+
+        action = 'followed you'
+
+    url = "https://exp.host/--/api/v2/push/send"
+    data = {
+        "to": creator.push_token,
+        "sound": "default",
+        "body": f'Somebody {action}',
+        "badge": 1
+    }
+    headers = {'Content-type': 'application/json',
+               'Accept': 'application/json', 'Accept-Encoding': 'gzip, deflate'}
+
+    request = requests.post(url, data=json.dumps(data), headers=headers)
+
+    print(request.json())
