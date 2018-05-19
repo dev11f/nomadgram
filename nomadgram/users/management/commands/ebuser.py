fb5b7eedf8217e6d61c21af146b1748d2c136b6c
HYOUNGBIN <jumice10@gmail.com>
2018-05-20 00:26:23 +0900

    superuser command added

---
+++ nomadgram/users/management/commands/ebuser.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+
+class command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        User = get_user_model()
+        User.objects.create_superuser('admin', 'admin@example.com', 'password')
